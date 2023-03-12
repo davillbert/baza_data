aaed1a61928f17b1d3dd4a04914e448f67dcccdb
davillbert <kiiamova.nr@phystech.edu>
2023-03-12 16:55:33 +0300

    ну вроде есть вторео задание и даже квадратные графиики и хз как норм сохранить табличку так что просто шакальный скрин в папке

--- lab2/main_2.py
+++ lab2/main_2.py
@@ -5,13 +5,14 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
-from prev_funcs import change_time, df_date2time, hhmmss2ms, df_date2time_interval
+from prev_funcs import change_time, df_date2time, hhmmss2ms
+from prettytable import PrettyTable
 
-os.chdir('lab2')
+#os.chdir('lab2') os.getcwd()+'\HistoryLog/
 
 
 START_TIME = '12:19:00.000'
-df_torque_data = pd.read_csv(os.getcwd()+'\HistoryLog/1210_УДМ_Y/2101_rw.torque.csv', delimiter=';')
+df_torque_data = pd.read_csv('HistoryLog/1210_УДМ_Y/2101_rw.torque.csv', delimiter=';')
 
 time_torque_df = pd.DataFrame(pd.concat([df_date2time(df_torque_data['Date and Time'], START_TIME), df_torque_data['torque']], axis=1),
                                 columns=['Date and Time', 'torque'])
@@ -20,7 +21,7 @@ time_torque_df.columns = ['Время, мс', 'Момент импульса, м
 # the_fig = px.line(time_torque_df, x='Время, мс', y='Момент импульса, м^2 кг/с', markers=False)
 # the_fig.show()
 
-START_PID0_TIME = '12:53:00.000'
+START_PID0_TIME = '12:53:29.470'
 FINISH_PID0_TIME = '12:54:25.749'
 
 START_PID1_TIME = '12:54:25.749'
@@ -35,11 +36,13 @@ FINISH_PID3_TIME = '13:06:29.069'
 START_PID4_TIME = '13:06:29.069'
 FINISH_PID4_TIME = '13:11:19.824'
 
-df_torque_data_for_pid = pd.read_csv('HistoryLog/1210_УДМ_Y/2101_rw.torque.csv', delimiter=';')
 
-time_torque_data_for_pid_df = pd.DataFrame(pd.concat([df_date2time(df_torque_data_for_pid['Date and Time'], START_PID0_TIME), df_torque_data_for_pid['torque']], axis=1),
-                               columns=['Date and Time', 'torque'])
-time_torque_data_for_pid_df.columns = ['Время, мс', 'Момент импульса, м^2 кг/с']
+
+df_torque_pid_data = pd.read_csv('HistoryLog/1210_УДМ_Y/2101_rw.torque.csv', delimiter=';')
+time_for_pid_torque_df = pd.DataFrame(pd.concat([df_date2time(df_torque_pid_data['Date and Time'], START_PID0_TIME),
+                                                 df_torque_pid_data['torque']], axis=1), columns=['Date and Time', 'torque'])
+
+time_for_pid_torque_df.columns = ['Время, мс', 'Момент импульса, м^2 кг/с']
 
 # ref_torq: (start_time, end_time)
 torq_map = {
@@ -76,27 +79,27 @@ time_map = {
         for k, v in torq_map.items()
 }
 
-# pid0 - 0.1 0.01 0
-# pid1 - 0.01 0.1 0
-# pid2 - 0.01 0.001 0
-# pid3 - 0.01 0.01 0.01
-# pid4 - 0.01 0 0.01
+# pid0 - P:0.1  I:0.01  D:0
+# pid1 - P:0.01 I:0.1   D:0
+# pid2 - P:0.01 I:0.001 D:0
+# pid3 - P:0.01 I:0.01  D:0.01
+# pid4 - P:0.01 I:0     D:0.01
 
 pid_torq_map = {
-        "pid0": (46380000,46465749),
-        "pid1": (46465749,46763750),
-        "pid2": (46763750,46910466),
-        "pid3": (46910466,47189069),
-        "pid4": (47189069,47479824)
+        "pid0": (0,56279),
+        "pid1": (56279,354280),
+        "pid2": (354280,500996),
+        "pid3": (500996,779599),
+        "pid4": (779599,47479824)
 
 }
 
 data_pid_map = {
-        k: np.array(time_torque_data_for_pid_df[
-            (time_torque_data_for_pid_df['Время, мс'] > v[0])
-            & (time_torque_data_for_pid_df['Время, мс'] < v[1])
-        ]['Момент импульса, м^2 кг/с'])
-        for k, v in pid_torq_map.items()
+    k: np.array(time_for_pid_torque_df[
+                    (time_for_pid_torque_df['Время, мс'] > v[0])
+                    & (time_for_pid_torque_df['Время, мс'] < v[1])
+                    ]['Момент импульса, м^2 кг/с'])
+    for k, v in pid_torq_map.items()
 }
 
 
@@ -124,6 +127,42 @@ ax.plot(list(data_map.keys()), std_arr, marker='o', markersize=8, color='black',
 # plt.show()
 fig.savefig('std(refT).png', dpi=150)
 
+
+fig.set_figheight(10)
+fig.set_figwidth(10)
+fig.savefig('std(refT_newsize).png', dpi=150)
+
+
+fig1, ax1 = plt.subplots()
+plt.title("Среднеквадратичное отклонение")
+plt.xlabel("Требуемое значение момента, м^2 кг/с")
+plt.ylabel("Среднеквадратичное отклонение в зависимости от PID")
+
+fig1.set_figheight(10)
+fig1.set_figwidth(10)
+plt.grid(True, axis='both')
+
+ax1.plot(list(data_pid_map.keys()), std_pid_arr, marker='o', markersize=8, color='black', linestyle='solid')
+fig1.savefig('std(refT1).png', dpi=150)
+
+th = ['number', 'P', 'I', 'D', 'CKO']
+td = ['0', '0.1', '0.01',  '0', std_pid_arr[0],
+      '1', '0.01', '0.1',  '0', std_pid_arr[1],
+      '2', '0.01', '0.001',  '0', std_pid_arr[2],
+      '3', '0.01', '0.01',  '0.01', std_pid_arr[3],
+      '4', '0.01', '0',  '0.01', std_pid_arr[4]]
+
+columns = len(th)
+
+table = PrettyTable(th)
+
+td_data = td[:]
+
+while td_data:
+    table.add_row(td_data[:columns])
+    td_data = td_data[columns:]
+
+print(table)
 
 
 print('Hello, bruh')
