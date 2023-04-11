976bc2ed583e79c58667db0f6c8f5de363424449
Марченко Арсений <1>
2023-04-11 17:52:42 +0300

    Почти сделал лабу 4

---
+++ lab4/funcs.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import numpy as np
+
+def hhmmss2ms(hms):
+    '''
+    hh:mm:ss.msmsms  ->  m_sec
+    '''
+    if '.' in hms and ':' in hms:
+        res = (int(hms[:2]) * 60 * 60 + int(hms[3:5])*60 + int(hms[6:8]))*1000 + int(hms[9:])
+    else:
+        return hhmmss2ms(f'{hms}.000')
+    return res 
+
+def df_date_and_time_to_time_in_ms(df_data, t_col='Date and Time', START_TIME='00:00:00'):
+    '''
+    Убирает дату, а время переводит в мс.
+    YY_MM_DD hh:mm:ss.msmsms  ->  hh:mm:ss.msmsms  ->  m_sec
+    '''
+    if '.' not in START_TIME and ':' in START_TIME:
+        return df_date_and_time_to_time_in_ms(df_data, t_col=t_col, START_TIME=f'{START_TIME}.000')
+    for i in range(len(df_data)):
+        if ' ' not in str(START_TIME):
+            df_data[t_col].at[i] = hhmmss2ms(df_data[t_col][i][9:]) - hhmmss2ms(START_TIME)
+        else:
+            df_data[t_col].at[i] = hhmmss2ms(df_data[t_col][i][9:]) - START_TIME
+
+    return df_data
+
+
+def cut_df(df, start, end, t_col='Date and Time', START_TIME='00:00:00'):
+    '''
+    Вырезает из таблицы пласт, ограниченный значениями start и end по значениям столбца t_col. 
+    df, int, int  ->  df_cutted
+    '''
+    res = df[(df[t_col] > (hhmmss2ms(start) - hhmmss2ms(START_TIME))) & (df[t_col] < (hhmmss2ms(end)- hhmmss2ms(START_TIME)))]
+    return res.reset_index().drop('index', axis=1)
+
+def cut_df_alot(df, time_brackets, START_TIME='00:00:00', t_col='Date and Time'):
+    '''
+    Вырезает из таблицы пласты, ограниченные значениями из time_brackets по значениям столбца t_col. 
+    Возвращает массив dataFrame'ов.
+    
+    df, [[int, int], ...]  ->  [df_cutted]
+    '''
+    res_array = []
+    for i in range(len(time_brackets)):
+        res_array.append(cut_df(df, time_brackets[i][0],  time_brackets[i][1], t_col=t_col, START_TIME=START_TIME))
+    return res_array
+
+
+def df_from_csv_w_cols(path, columns, delimiter=';'):
+    '''
+    Из csv файла path возвращает dataFrame со столбцами columns.
+    '''
+    df = pd.read_csv(path, header=0, usecols=columns, delimiter=';')
+    
+    return df
+
+
+def get_vec_abs_from_components(df, xyz_cols, t_col='Date and Time', vec_name='Abs(Vector)'):
+    '''
+    Возвращает dataFrame с модулем вектора, который задаётся несколькими компонентами. Названия столбцов компонент в df передаётся в виде массива xyz_cols.
+
+    dataFrame, []  ->  dataFrame:[Time, Vec]
+    '''
+    squares_sum = np.zeros(len(df[xyz_cols[0]]))
+    for i in range(len(xyz_cols)):
+        for j in range(len(df[xyz_cols[i]])):
+            squares_sum[j] += (df[xyz_cols[i]][j]) ** 2
+    for i in range(len(squares_sum)):
+        squares_sum[i] = np.sqrt(squares_sum[i])
+
+    vec_df = pd.DataFrame(squares_sum, columns=[vec_name])
+    
+    new_df = pd.concat([df[t_col], vec_df], axis=1, ignore_index=True)
+    new_df.columns = [t_col, vec_name]
+    return new_df
+
+
+def get_std_of_vec(df, vec_name):
+    '''
+    Возвращает СКО/СР.ЗНАЧ от всех значений столбца vec_name в df.
+    df, str  ->  float
+    '''
+    arr = np.array(df[vec_name])
+    std = arr.std()/arr.mean()
+    return std
+
+
+def get_ampl_of_vec(df, vec_name):
+    '''
+    Возвращает амплитуду (max-min / 2) от всех значений столбца vec_name в df.
+    df, str  ->  float
+    '''
+    arr = np.array(df[vec_name])
+    return (arr.max() - arr.min()) / 2
---
+++ lab4/main4.py
@@ -0,0 +1,65 @@
+# from lab2.prev_funcs import hhmmss2ms, df_date2time, change_time
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from funcs import (cut_df, cut_df_alot, df_date_and_time_to_time_in_ms,
+                   df_from_csv_w_cols, get_ampl_of_vec, get_std_of_vec,
+                   get_vec_abs_from_components, hhmmss2ms)
+
+path = 'lab4/HistoryLog_2023-03-21_11-34-12/1008_Магнитометр/2903_device.regular1.csv'
+columns = ['Date and Time', 'regular1_data.mag.x', 'regular1_data.mag.y', 'regular1_data.mag.z']
+xyz_cols = ['regular1_data.mag.x', 'regular1_data.mag.y', 'regular1_data.mag.z']
+t_col = 'Date and Time'
+vec_name = 'B'
+
+START_TIME = '11:39:00'
+time_brackets = [
+    ['11:39:09', '11:39:29'],
+    ['11:40:04', '11:40:24'],
+    ['11:40:39', '11:40:59'],
+    ['11:41:00', '11:41:20'],
+    ['11:41:31', '11:41:51']
+    ]
+param_name = 'Angular Rate'
+param_values = ['200', '400', '600', '800', '1000']
+
+df = df_from_csv_w_cols(path, columns)
+df = df_date_and_time_to_time_in_ms(df, t_col=t_col, START_TIME=START_TIME)
+B_df = get_vec_abs_from_components(df, xyz_cols, t_col=t_col, vec_name=vec_name)
+
+B_dfs_array = cut_df_alot(B_df, time_brackets, START_TIME, t_col)
+
+std_array = []
+ampl_array = []
+for df_i in B_dfs_array:
+    std_array.append(get_std_of_vec(df_i, vec_name))
+    ampl_array.append(get_ampl_of_vec(df_i, vec_name))
+
+
+fig1 = plt.figure()
+fig1.suptitle("Влияние скорости вращения УДМ на показания магнитометра.")
+fig1.set_figheight(10)
+fig1.set_figwidth(10)
+
+ax1_1 = fig1.add_subplot(2,1,1)
+ax1_2 = fig1.add_subplot(2,1,2)
+
+ax1_1.plot(param_values, std_array, marker='o', markersize=8, color='black', linestyle='solid')
+ax1_1.set_title("Среднеквадратичное отклонение", fontsize=10)
+ax1_1.set(xlabel=param_name, 
+            ylabel="Среднеквадратичное отклонение,\n нормированное на единицу")
+ax1_1.grid(True, axis='both')
+
+ax1_2.plot(param_values, ampl_array, marker='o', markersize=8, color='black', linestyle='solid')
+ax1_2.set(title="Амплитуда", xlabel=param_name, 
+            ylabel="Амплитуда $(max-min)/2$")
+ax1_2.grid(True, axis='both')
+
+# fig1.savefig('std(refT).png', dpi=150)
+plt.show()
+
+
+print('Hello')
